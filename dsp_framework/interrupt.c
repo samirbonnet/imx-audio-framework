c2aae0e4139338fd8c0faa4008d28be654b426f7
Samir Bonnet <samir.bonnet@outlook.fr>
2023-05-06 23:23:51 +0200

    refactor 3

--- dsp_framework/interrupt.c
+++ dsp_framework/interrupt.c
@@ -1,7 +1,14 @@
 #include "interrupt.h"
 
+uint32_t elapsed = 0;
 
 void ISR()
 {
+    uint32_t begin = xthal_get_ccount();
 
+
+
+
+
+    elapsed = xthal_get_ccount() - begin;
 }
--- dsp_framework/interrupt.h
+++ dsp_framework/interrupt.h
@@ -1,6 +1,10 @@
 #ifndef INTERRUPT_H
 #define INTERRUPT_H
 
+#include <stdint.h>
+
+extern uint32_t elapsed;
+
 void ISR();
 
 #endif
